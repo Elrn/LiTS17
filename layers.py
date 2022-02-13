9f3570649aa00fa38d62913fde5444d94b79ec64
Elrn <sona@pm.me>
2022-02-14 00:45:46 +0900

    method 명 변경

--- layers.py
+++ layers.py
@@ -8,13 +8,14 @@ BN_ACT = lambda x : tf.nn.relu(BatchNormalization()(x))
 ACT_BN = lambda x : BatchNormalization()(tf.nn.relu(x))
 
 ########################################################################################################################
-class Attention(tf.keras.layers.Layer):
+class sep_bias(tf.keras.layers.Layer):
     def __init__(self, input_dims):
-        super(Attention, self).__init__()
+        super(sep_bias, self).__init__()
         assert input_dims > 0
         self.input_dims = input_dims
 
     def build(self, input_shape):
+        print(input_shape)
         self.scale = Embedding(self.input_dims, input_shape[-1], embeddings_initializer='ones')
         self.offset = Embedding(self.input_dims, input_shape[-1], embeddings_initializer='zeros')
 
@@ -41,7 +42,7 @@ def encoder(filters, kernel=3, pool=2):
 
     def main(inputs):
         x = Conv3D(filters, kernel, padding='same')(inputs)
-        x = Attention(1)(x)
+        x = sep_bias(1)(x)
         x = conv_bn_act(x)
         x = conv_bn_act(x)
         x = AveragePooling3D(pool, pool)(x)
@@ -52,7 +53,7 @@ def encoder(filters, kernel=3, pool=2):
 def decoder(filters, div=4, kernel=3):
     concat_list = []
     div_channel = filters // div
-    attn = Attention(div)
+    attn = sep_bias(div)
     def main(x):
         features = [Conv3D(div_channel, 1)(attn(x, i)) for i in range(div)]
         for feature in features:
@@ -70,7 +71,7 @@ def bottle_neck(filters, div=4, kernel=3):
     concat_list = []
     dilation_rate = [1+2*i for i in range(div)]
     div_channel = filters // div
-    attn = Attention(div)
+    attn = sep_bias(div)
     def main(x):
         features = [Conv3D(div_channel, 1)(attn(x, i)) for i in range(div)]
         for i, feature in enumerate(features):
