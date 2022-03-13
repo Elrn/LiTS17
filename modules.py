51d8a0c330c2a52bf110f67f564808fe9d2e8d27
Elrn <sona@pm.me>
2022-03-13 21:02:16 +0900

    ADD 2D, 3D branch

--- modules.py
+++ modules.py
@@ -4,20 +4,27 @@ from layers import *
 
 ########################################################################################################################
 act = 'relu'
+D = 2
+if D == 2:
+    conv = Conv2D
+    convTranspose = Conv2DTranspose
+elif D == 3:
+    conv = Conv3D
+    convTranspose = Conv3DTranspose
 
 BN_ACT = lambda x : tf.nn.relu(BatchNormalization()(x))
 ACT_BN = lambda x : BatchNormalization()(tf.nn.relu(x))
 ########################################################################################################################
 def encoder(filters, kernel=3, pool=2):
     def conv_bn_act(x):
-        x = Conv3D(filters, kernel, padding='same', groups=filters)(x)
-        x = Conv3D(filters, kernel, padding='same', groups=filters)(x)
+        x = conv(filters, kernel, padding='same', groups=filters)(x)
+        x = conv(filters, kernel, padding='same', groups=filters)(x)
         x = BatchNormalization()(x)
         x = Activation(act)(x)
         return x
 
     def main(inputs):
-        x = Conv3D(filters, kernel, padding='same')(inputs)
+        x = conv(filters, kernel, padding='same')(inputs)
         # x = ACT_BN(x)
         x = sep_bias(1)(x)
         x = conv_bn_act(x)
@@ -32,11 +39,11 @@ def decoder(filters, div=4, kernel=3):
     div_channel = filters // div
     attn = sep_bias(div)
     def main(x):
-        features = [Conv3D(div_channel, 1)(attn(x, i)) for i in range(div)]
+        features = [conv(div_channel, 1)(attn(x, i)) for i in range(div)]
         for feature in features:
-            x = Conv3DTranspose(div_channel, kernel, strides=2, padding='same')(feature)
+            x = convTranspose(div_channel, kernel, strides=2, padding='same')(feature)
             for j in range(1, 4):
-                x += Conv3D(div_channel, kernel, dilation_rate=j, padding='same', groups=div_channel)(x)
+                x += conv(div_channel, kernel, dilation_rate=j, padding='same', groups=div_channel)(x)
             concat_list.append(x)
         x = tf.concat(concat_list, -1)
         x = BN_ACT(x)
@@ -50,10 +57,10 @@ def bottle_neck(filters, div=4, kernel=3):
     div_channel = filters // div
     attn = sep_bias(div)
     def main(x):
-        features = [Conv3D(div_channel, 1)(attn(x, i)) for i in range(div)]
+        features = [conv(div_channel, 1)(attn(x, i)) for i in range(div)]
         for i, feature in enumerate(features):
-            x = Conv3D(div_channel, kernel, dilation_rate=dilation_rate[i], padding='same')(feature)
-            x = Conv3D(div_channel, kernel, dilation_rate=dilation_rate[i], padding='same', groups=div_channel)(x)
+            x = conv(div_channel, kernel, dilation_rate=dilation_rate[i], padding='same')(feature)
+            x = conv(div_channel, kernel, dilation_rate=dilation_rate[i], padding='same', groups=div_channel)(x)
             if concat_list:
                 x = tf.add(concat_list[-1], x)
             concat_list.append(x)
