e292e078de906d79e1f7ab5f077f106b96644fc0
Elrn <sona@pm.me>
2022-03-18 21:31:17 +0900

    Connect skip connection

--- models.py
+++ models.py
@@ -17,22 +17,21 @@ def SR(n_class, base_filters=64, depth=2):
     return main
 
 ########################################################################################################################
-def AE(n_class, base_filters=64, depth=2):
+def AE(n_class, base_filters=64, depth=3):
     def main(inputs):
         filters = [base_filters*i for i in range(1, depth+2)]
         x = inputs
-        # skip_conn_list = []
+        skip_conn_list = []
         ### Encoder
         for i in range(depth):
-            x = modules.encoder(filters[i])(x)
-            # skip = skip_conn_fn(filters[i])(skip)
-            # skip_conn_list.append(skip)
+            x, skip = modules.encoder(filters[i])(x)
+            skip = modules.skip_connection(filters[i])(skip)
+            skip_conn_list.append(skip)
         ### BottleNeck
         x = modules.bottle_neck(filters[-1])(x)
         ### Decoder
         for i in reversed(range(depth)):
-            # x = decoder(filters[i])(x, skip_conn_list[i])
-            x = modules.decoder(filters[i])(x)
+            x = modules.decoder(filters[i])(x, skip_conn_list[i])
         x = modules.conv(n_class, 1)(x)
         output = Softmax(-1)(x)
         return output
