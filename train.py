b0776f3bcee734a9c952407c84c4c8438b8afd59
Elrn <sona@pm.me>
2022-03-18 21:32:22 +0900

    -

--- train.py
+++ train.py
@@ -1,11 +1,13 @@
 #-*- coding: utf-8 -*-
 
 import tensorflow as tf
-import utils, models, metrics, callbacks
-from Data import LiST17
+import utils, models, metrics, callbacks, losses
+from Data import LiST17, LiST17_2D
 from tensorflow.keras.callbacks import *
 import os, re, logging
 
+# import tensorflow.experimental.numpy as tnp
+# tnp.experimental_enable_numpy_behavior()
 ########################################################################################################################
 os.putenv('TF_GPU_ALLOCATOR', 'cuda_malloc_async')
 os.environ['CUDA_VISIBLE_DEVICES'] = '0'
@@ -32,14 +34,14 @@ paths = [utils.join_dir([base_dir, 'log', dir]) for dir in dirs]
 [utils.mkdir(path) for path in paths]
 plt_dir, ckpt_dir = paths
 
-# ckpt_file_name = 'EP_{epoch}, L_{loss:.3f}, P_{Precision:.3f}, R_{Recall:.3f}, vP_{val_Precision:.3f}, vR_{val_Recall:.3f}.hdf5'
-ckpt_file_name = 'EP_{epoch}, L_{loss:.4f}, vL_{val_loss:.4f}.hdf5'
+ckpt_file_name = 'EP_{epoch}, L_{loss:.3f}, P_{Precision:.3f}, R_{Recall:.3f}, vP_{val_Precision:.3f}, vR_{val_Recall:.3f}.hdf5'
+# ckpt_file_name = 'EP_{epoch}, L_{loss:.4f}, vL_{val_loss:.4f}.hdf5'
 ckpt_file_path = utils.join_dir([ckpt_dir, ckpt_file_name])
 
 ### Get Data
-dataset, val_dataset = LiST17.build(batch_size=10, validation_split=0.2) # [0]:train [1]:valid or None
-test_dataset = LiST17.build_test(10)
-num_class, input_shape = LiST17.num_class, LiST17.input_shape
+dataset, val_dataset = LiST17_2D.build(batch_size=10, validation_split=0.2) # [0]:train [1]:valid or None
+test_dataset = LiST17_2D.build_test(10)
+num_class, input_shape = LiST17_2D.num_class, LiST17_2D.input_shape
 
 ### Build model
 input = tf.keras.layers.Input(shape=input_shape)
@@ -54,9 +56,9 @@ metric_list = [
     metrics.DSC(num_class),
     metrics.JSC(num_class),
 ]
-model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.0000001),
-              loss='CategoricalCrossentropy',
-              # metrics=metric_list,
+model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.0001),
+              loss=losses.segCE(),
+              metrics=metric_list,
               )
 
 ### load weights
@@ -75,13 +77,14 @@ else:
 ### Train model
 history = model.fit(
     x = dataset,
-    epochs=initial_epoch+10,
+    epochs=100,
     validation_data=val_dataset,
     initial_epoch=initial_epoch,
     callbacks=[
         ModelCheckpoint(ckpt_file_path, monitor='loss', save_best_only=True, save_weights_only=False, save_freq='epoch'),
         # EarlyStopping(monitor='loss', min_delta=0, patience=5),
-        ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=2, verbose=0, min_delta=0.0001, cooldown=0, min_lr=0),
+        # ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=2, verbose=0, min_delta=0.0001, cooldown=0, min_lr=0),
+        callbacks.setLR(0.00004),
         callbacks.monitor(plt_dir, dataset=test_dataset)
     ]
 )
