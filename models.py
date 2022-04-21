214ad3da0ef823387bd48c047d2d3531808f4ae6
Elrn <sona@pm.me>
2022-04-21 22:32:04 +0900

    AGLN 모델 추가

--- models.py
+++ models.py
@@ -38,3 +38,26 @@ def AE(n_class, base_filters=64, depth=3):
     return main
 
 ########################################################################################################################
+
+def AGLN(n_class, base_filters=32, depth=3, compression_rate=0.6):
+    """
+    Attention Guided Global Enhancement and Local Refinement Network for Semantic Segmentation
+    :return:
+    """
+    def main(x):
+        filters = [base_filters * i for i in range(1, depth + 2)]
+        skips = []
+        ### Encoder
+        for i in range(depth):
+            x, skip = modules.encoder(filters[i])(x)
+            skips.append(modules.skip_connection(filters[i])(skip))
+        ### BottleNeck
+        x = modules.bottle_neck(filters[-1])(x)
+        D, Bs = modules.sematic_aggregation_block(filters, compression_rate)(x)
+        ### Decoder
+        for i in reversed(range(depth)):
+            D = modules.context_fusion_block(filters[i], i=i)(skips[i], D, Bs[i])
+        x = modules.conv(n_class, 1)(D)
+        output = Softmax(-1)(x)
+        return output
+    return main
--- modules.py
+++ modules.py
@@ -16,6 +16,9 @@ elif D == 3:
 
 BN_ACT = lambda x : tf.nn.relu(BatchNormalization()(x))
 ACT_BN = lambda x : BatchNormalization()(tf.nn.relu(x))
+
+########################################################################################################################
+# Encoder
 ########################################################################################################################
 def encoder(filters, kernel=3, pool=2):
     def conv_bn_act(x):
@@ -36,6 +39,8 @@ def encoder(filters, kernel=3, pool=2):
     return main
 
 ########################################################################################################################
+# Decoder
+########################################################################################################################
 def decoder(filters, div=4, kernel=3):
     concat_list = []
     div_channel = filters // div
@@ -53,6 +58,55 @@ def decoder(filters, div=4, kernel=3):
         return x
     return main
 
+def context_fusion_block(filters, kernel=3, **kwargs):
+    """
+    Attention Guided Global Enhancement and Local Refinement Network for Semantic Segmentation
+    :return:
+    """
+    def semantic_distribution_module(filters, kernel=3):
+        """
+        :return: [B, H, W, C], [B, H, W, C]
+        """
+        def main(D, B): # [] / [B, N, C]
+            D = convTranspose(filters, kernel, strides=2, padding='same', name=f"convt_{kwargs['i']}")(D)
+            M = flat()(conv(B.shape[1], 1, padding='same')(D))
+            M = tf.matmul(M, B) # B, I, C
+
+            print(D.shape, M.shape)
+
+            axis = [-1] + list(D.shape[1:])
+            M = tf.reshape(M, axis) # B, H, W, C
+
+            S = conv(filters, kernel, padding='same')(D + M)
+            S = BatchNormalization()(S)
+            S = tf.nn.relu(S)
+            return S, M
+        return main
+
+    def local_refinement_module():
+        """
+        :return: [B, H, W, C]
+        """
+        flatten = flat()
+        def main(E, S, M):
+            channel_attention = tf.matmul(flatten(E), flatten(S), transpose_a=True) # B, C, C
+            F = tf.matmul(flatten(E), channel_attention)
+            F = tf.reshape(F, [-1] + list(E.shape[1:]))
+            G = F * M
+            return G
+        return main
+
+    def main(E, D, B):
+        S, skip = semantic_distribution_module(filters)(D, B)
+        G = local_refinement_module()(E, S, skip)
+        x = conv(filters, kernel, padding='same')(S+G)
+        x = BatchNormalization()(x)
+        x = tf.nn.relu(x)
+        return x
+    return main
+
+########################################################################################################################
+# bottle-neck
 ########################################################################################################################
 def bottle_neck(filters, div=4, kernel=3):
     concat_list = []
@@ -72,6 +126,22 @@ def bottle_neck(filters, div=4, kernel=3):
         return x
     return main
 
+def sematic_aggregation_block(filters:list, compression_rate):
+    """
+    Attention Guided Global Enhancement and Local Refinement Network for Semantic Segmentation
+    :return: ([B, H, W, C], [B, N, C]:list)
+    """
+    flatten = flat()
+    def main(inputs):
+        X = [flatten(conv(f, 1)(inputs)) for f in filters] # [B, HW, C]
+        X_t = [flatten(conv(int(f*compression_rate), 1)(inputs)) for f in filters] # [B, HW, N]
+
+        Bs = [tf.matmul(x_t, x, transpose_a=True) for x_t, x in zip(X_t, X)] # [B, N, C]
+        return inputs, Bs
+    return main
+
+########################################################################################################################
+# skip-connection
 ########################################################################################################################
 def skip_connection(filters, kernel=5):
     def main(x):
